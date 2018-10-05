c52325c123f053da578275b226a1ea738bf93a8a
Chris <chris.ruegger@gmail.com>
2018-10-05 06:44:22 -0400

    Initial

---
+++ src/main/java/com/rueggerllc/cuke/Cuke1Application.java
@@ -0,0 +1,12 @@
+package com.rueggerllc.cuke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cuke1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cuke1Application.class, args);
+	}
+}
---
+++ src/test/resources/__files/belly.feature
@@ -0,0 +1,7 @@
+Feature: Belly
+  Exercise the Belly Feature
+  
+Scenario: a few cukes
+  Given I have 42 cukes in my belly
+  When I wait 1 hour
+  Then my belly should growl
